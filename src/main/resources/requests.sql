89e498734a436d930ee5b3fcef24fd2dfbfb6fdf
arakhmatullin <arakhmatullin@cloudally.com>
2019-11-07 20:42:39 +0300

    fixes: #2 fixes: #3

--- src/main/resources/requests.sql
+++ src/main/resources/requests.sql
@@ -1,29 +1,28 @@
 DROP TABLE categories;
 CREATE TABLE categories
 (
-    id        INTEGER PRIMARY KEY AUTOINCREMENT,
-    name      TEXT NOT NULL,
-    parent_id INTEGER
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    name     TEXT NOT NULL,
+    parentId INTEGER REFERENCES categories (id)
 );
 
 
 DROP TABLE products;
 CREATE TABLE products
 (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    name        TEXT    NOT NULL,
-    category_id INTEGER NOT NULL
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    name       TEXT    NOT NULL,
+    categoryId INTEGER NOT NULL REFERENCES categories (id)
 );
 
 DROP TABLE store;
 CREATE TABLE store
 (
-    id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    product_id INTEGER,
-    price      INTEGER CHECK ( price >= 0 ),
-    quantity   INTEGER CHECK ( quantity >= 0 ),
-    status     INTEGER --; 0 - Unavailable , 1 - Available
-
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    productId INTEGER REFERENCES products (id),
+    price     INTEGER CHECK ( price >= 0 ),
+    quantity  INTEGER CHECK ( quantity >= 0 ),
+    status    INTEGER --; 0 - Unavailable , 1 - Available
 );
 
 DROP TABLE users;
@@ -42,7 +41,7 @@ DROP TABLE orders;
 CREATE TABLE orders
 (
     id             INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id        INTEGER,
+    userId         INTEGER REFERENCES users (id),
     orderDate      INTEGER CHECK ( orderDate > 0 ),
     deliveryAdders TEXT NOT NULL,
     status         INTEGER --; 0  Finished, 1 Paid, 2 Unpaid, 3 Canceled
@@ -53,42 +52,42 @@ DROP TABLE orderProducts;
 CREATE TABLE orderProducts
 (
     id       INTEGER PRIMARY KEY AUTOINCREMENT,
-    order_id INTEGER NOT NULL,
-    store_id INTEGER NOT NULL,
+    orderId  INTEGER NOT NULL REFERENCES orders (id),
+    storeId  INTEGER NOT NULL REFERENCES store (id),
     quantity INTEGER CHECK ( quantity > 0 )
 );
 
 
-INSERT INTO categories (name, parent_id)
+INSERT INTO categories (name, parentId)
 VALUES ("Автотовары и автозапчасти", 0);
-INSERT INTO categories (name, parent_id)
+INSERT INTO categories (name, parentId)
 VALUES ("Автомобильный звук и видео", 1);
-INSERT INTO categories (name, parent_id)
+INSERT INTO categories (name, parentId)
 VALUES ("Автомагнитолы", 2);
-INSERT INTO categories (name, parent_id)
+INSERT INTO categories (name, parentId)
 VALUES ("Автомагнитолы Pioneer", 3);
-INSERT INTO categories (name, parent_id)
+INSERT INTO categories (name, parentId)
 VALUES ("Автомагнитолы ACV", 3);
 
-INSERT INTO products (name, category_id)
+INSERT INTO products (name, categoryId)
 VALUES ("Автомагнитола Pioneer FH-X730BT", 4);
-INSERT INTO products (name, category_id)
+INSERT INTO products (name, categoryId)
 VALUES ("Автомагнитола Pioneer DEH-S110UB", 4);
-INSERT INTO products (name, category_id)
+INSERT INTO products (name, categoryId)
 VALUES ("Автомагнитола ACV AVS-1711GD", 5);
 
-INSERT INTO store (product_id, price, quantity, status)
+INSERT INTO store (productId, price, quantity, status)
 VALUES (1, 7000, 10, 0);
-INSERT INTO store (product_id, price, quantity, status)
+INSERT INTO store (productId, price, quantity, status)
 VALUES (1, 9499, 10, 1);
-INSERT INTO store (product_id, price, quantity, status)
+INSERT INTO store (productId, price, quantity, status)
 VALUES (2, 4150, 10, 1);
-INSERT INTO store (product_id, price, quantity, status)
+INSERT INTO store (productId, price, quantity, status)
 VALUES (3, 1199, 10, 1);
 
-UPDATE store;
+UPDATE store
 SET quantity =0
-WHERE product_id == 1
+WHERE productId == 1
   and price == 7000;
 
 INSERT INTO users (firstName, middleName, lastName, phone, deliveryAdders)
@@ -108,15 +107,15 @@ DELETE
 FROM users
 WHERE id == 1;
 
-INSERT INTO orders (user_id, orderDate, deliveryAdders, status)
+INSERT INTO orders (userId, orderDate, deliveryAdders, status)
 VALUES (2, 1572734758, "Bobruisk", 0);
-INSERT INTO orders (user_id, orderDate, deliveryAdders, status)
+INSERT INTO orders (userId, orderDate, deliveryAdders, status)
 VALUES (3, 1572720758, "Moscow,Kremlin", 0);
 
-INSERT INTO orderProducts (order_id, store_id, quantity)
+INSERT INTO orderProducts (orderId, storeId, quantity)
 VALUES (1, 3, 1);
-INSERT INTO orderProducts (order_id, store_id, quantity)
+INSERT INTO orderProducts (orderId, storeId, quantity)
 VALUES (1, 4, 1);
-INSERT INTO orderProducts (order_id, store_id, quantity)
+INSERT INTO orderProducts (orderId, storeId, quantity)
 VALUES (2, 2, 5);
 
